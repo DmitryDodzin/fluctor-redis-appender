b5115d0af9715d29f2e7d6c8d5d6044b5a233be0
Dmitry Dodzin <d.dodzin@gmail.com>
2017-06-08 13:58:45 +0300

    Adding the metadata as export for the Appender

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
+const package = require('./package.json');
+
+const METADATA = {
+  NAME: 'redis',
+  VERSION: package.version,
+  API_VERSION: 1,
+  SUPPORT: 0x00000111
+};
+
 const appender = require('./lib/appender');
 
 module.exports = {
+  METADATA,
   appender
 };
